2800a4dcda38fd7808d7996fc776f5702d0432c8
Mohamad Aid Bacuk Zanab <mohammedsharif067@gmail.com>
2023-05-11 00:01:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const thanksMessages = [];
+    for(let i=0; i < names.length; i++) {
+        const thanksMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        thanksMessages.push(thanksMessage);
+    }
+    return thanksMessages;
+}
+
+
+const thanks = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(thanks);
+
+
+
+function countDown(number) {
+    let countdown = number;
+    while (countdown >= 0) {
+        console.log(countdown--);
+    }
+}
+
+
+countDown(10);
